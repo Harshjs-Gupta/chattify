ac4a99041b878a9c10a00030cb9164f7ba278466
Harsh Gupta <harshgupta88911@gmail.com>
2026-07-05 09:03:01 +0530

    Initial commit :rocket:

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDm1ThjPIWnCvMJ_uTceOcPLtLrWRW_Qjg",
-  authDomain: "chattify-7174d.firebaseapp.com",
-  projectId: "chattify-7174d",
-  storageBucket: "chattify-7174d.appspot.com",
-  messagingSenderId: "850052492435",
-  appId: "1:850052492435:web:ade059c6245f34db1e7166",
-  measurementId: "G-GET407LTWT",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
